bdb08d800b235ca2709ca2c167221a5fa475e77c
simiyutin <simiyutin.boris@yandex.ru>
2026-03-31 11:51:32 +0300

    updated ceres solver install script to not throw error if already exists

--- .github/scripts/linux/install_ceres_solver.sh
+++ .github/scripts/linux/install_ceres_solver.sh
@@ -6,7 +6,7 @@ sudo apt install libunwind-dev libgoogle-glog-dev libgflags-dev libatlas-base-de
 wget https://github.com/ceres-solver/ceres-solver/archive/2.2.0.zip
 unzip 2.2.0.zip
 cd ceres-solver-2.2.0
-mkdir build && cd build
+mkdir -p build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=/opt/ceres-solver220 -DUSE_CUDA=OFF ..
 make -j$(nproc)
 sudo make install
--- .github/scripts/macos/install_ceres_solver.sh
+++ .github/scripts/macos/install_ceres_solver.sh
@@ -6,7 +6,7 @@ brew install eigen glog gflags suite-sparse
 wget https://github.com/ceres-solver/ceres-solver/archive/2.2.0.zip
 unzip 2.2.0.zip
 cd ceres-solver-2.2.0
-mkdir build && cd build
+mkdir -p build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=/opt/ceres-solver220 -DUSE_CUDA=OFF ..
 make -j$(sysctl -n hw.ncpu)
 sudo make install
